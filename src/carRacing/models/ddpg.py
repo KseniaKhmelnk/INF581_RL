7848746875f52ab101a05dca93cf85d55a3c43ab
OvsPolina <58885435+OvsPolina@users.noreply.github.com>
2024-03-28 19:24:29 +0100

    Update ddpg.py

--- src/carRacing/models/ddpg.py
+++ src/carRacing/models/ddpg.py
@@ -1,5 +1,5 @@
 from carRacing.models.abstract import CarRacingModel
-from carRacing.models.ddpg_base import Actor, Critic, ReplayBuffer, Transition
+from carRacing.models.ddpg_base import DDPG, Transition
 import numpy as np 
 import copy 
 import torch
@@ -17,26 +17,27 @@ class DDPG(CarRacingModel):
         self.action_bound = action_bound
         
         # untrained
-        self.actor = Actor(self.state_dim, self.action_dim, self.action_bound)
+        #self.actor = Actor(self.state_dim, self.action_dim, self.action_bound)
+        agent = DDPG(state_dim, action_dim)
 
     def predict(self, observation: np.ndarray) -> np.ndarray:
         state_tensor = torch.tensor(observation, dtype=torch.float32, device=self.device).unsqueeze(0)
         with torch.no_grad():
-            action = self.actor(state_tensor).squeeze(0)
+            action = self.agent.actor(state_tensor).squeeze(0)
         action = action.cpu().numpy()    
         action = np.clip(action + np.random.normal(0, 0.1), -self.action_bound, self.action_bound)
         return action
     
     def load(self, model_path: str) -> None:
-        self.actor.load_state_dict(torch.load(model_path))
+        self.agent.actor.load_state_dict(torch.load(model_path))
 
     def save(self, model_path: str = "ddpg.pt") -> None:
-        torch.save(self.actor.state_dict(), model_path)
+        torch.save(self.agent.actor.state_dict(), model_path)
 
     def train(
       self,
-      num_episodes = 100,
-      max_steps_per_episode = 5, # Максимальное количество шагов в эпизоде
+      num_episodes = 10,
+      max_steps_per_episode = 5000, 
       init_step = 50,
       batch_size = 5,
       gamma = 0.99,
@@ -47,29 +48,15 @@ class DDPG(CarRacingModel):
         
         env = self.make_env()
 
-        # Определение параметров
         state_dim = self.state_dim
         action_dim = self.action_dim
         action_bound = self.action_bound
 
-        # Определение экземпляров сетей Actor и Critic
-        actor = Actor(state_dim, action_dim, action_bound)
-        critic = Critic(state_dim, action_dim)
+        #actor = Actor(state_dim, action_dim, action_bound)
+        #critic = Critic(state_dim, action_dim)
+        agent = DDPG(state_dim, action_dim)
 
-
-        # Создание целевой модели актора и копирование весов из основной модели
-        target_actor = copy.deepcopy(actor)
-        target_actor.eval()
-        
-        target_critic = copy.deepcopy(critic)
-        target_critic.eval()
-
-        # Определение оптимизаторов
-        actor_optimizer = torch.optim.Adam(actor.parameters(), lr=actor_lr)
-        critic_optimizer = torch.optim.Adam(critic.parameters(), lr=critic_lr)
-
-        # Определение буфера воспроизведения
-        replay_buffer = ReplayBuffer(capacity=10000)
+        episode_rewards = []
 
         for episode in range(num_episodes):
             state, _ = env.reset()
@@ -86,10 +73,12 @@ class DDPG(CarRacingModel):
                 state_tensor = torch.tensor(state, dtype=torch.float32, device=self.device).unsqueeze(0)
 
                 with torch.no_grad():
-                    action = actor(state_tensor).squeeze(0)
+                    #action = actor(state_tensor).squeeze(0)
+                    action = agent.random_action()
 
-                print("Original action:", action)
-                action = action.cpu().numpy()    
+                #print("Original action:", action)
+                #action = action.cpu().numpy()    
+                #action = np.clip(action + np.random.normal(0, 0.1), -action_bound, action_bound)
                 action = np.clip(action + np.random.normal(0, 0.1), -action_bound, action_bound)
                 
 
@@ -100,46 +89,32 @@ class DDPG(CarRacingModel):
                 print(f"Action: {action}, Reward: {episode_reward}, Done: {done}")
 
                 transition = (state, action, next_state, reward, done)
-                replay_buffer.push(transition)
+                agent.replay_buffer.push(transition)
                 episode_reward += reward
 
-                if len(replay_buffer.memory) > batch_size:
-                    batch = replay_buffer.sample(batch_size)
-                    batch = Transition(*zip(*batch))
+                if step % 1000 == 0:
+                    agent.save_model()
 
-                    state_batch = torch.tensor(batch.state, dtype=torch.float32)
-                    action_batch = torch.tensor(batch.action, dtype=torch.float32)
-                    next_state_batch = torch.tensor(batch.next_state, dtype=torch.float32)
-                    reward_batch = torch.tensor(batch.reward, dtype=torch.float32).unsqueeze(1)
-                    done_batch = torch.tensor(batch.done, dtype=torch.float32).unsqueeze(1)
-
-                    with torch.no_grad():
-                        target_action_next = target_actor(next_state_batch)
-                        target_q_next = target_critic(next_state_batch, target_action_next)
-                        target_q = reward_batch + gamma * (1.0 - done_batch) * target_q_next
-
-                    critic_loss = F.mse_loss(critic(state_batch, action_batch), target_q)
-                    print(f"Critic loss: {critic_loss.item()}, Step: {step}, Episode: {episode}")
-
-                    critic_optimizer.zero_grad()
-                    critic_loss.backward()
-                    critic_optimizer.step()
-
-                    predicted_action = actor(state_batch)
-                    actor_loss = -critic(state_batch, predicted_action).mean()
-
-                    actor_optimizer.zero_grad()
-                    actor_loss.backward()
-                    actor_optimizer.step()
+                if len(agent.replay_buffer.memory) > agent.batch_size:
+                    agent.update_policy()
 
                 state = next_state
                 if done:
-                    print("Done")
+                    #print("Done")
                     print(f"Episode: {episode + 1}, Reward: {episode_reward}")
                     break
 
-        # Закрытие среды
+            episode_rewards.append(episode_reward)
+
+        agent.save_model()
+
         env.close()
 
-        self.actor = actor
+        plt.plot(episode_rewards)
+        plt.title("Episode Rewards")
+        plt.xlabel('Episode')
+        plt.ylabel('Total Reward')
+        plt.show()
+
+        self.agent = agent
 
